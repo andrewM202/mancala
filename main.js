60e5b2fb29417cbf190c19b8468fb045d4801c98
Andrew Matt <andrewm1246@gmail.com>
2020-12-03 11:45:39 -0500

    Added script tag in html file, and made a function to randomly pick starting player

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let playerOneTurn = false;
+let playerTwoTurn = false;
 
+function randomPlayer() {
+  if(Math.random() < 0.5) {
+    playerOneTurn = true;
+  } else {
+    playerTwoTurn = true;
+  }
+}
+
+window.onload = function() {
+  randomPlayer();
+  console.log(`Player One: ${playerOneTurn}`);
+  console.log(`Player Two: ${playerTwoTurn}`);
+}
