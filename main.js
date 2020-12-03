8949bef1908e135d1e36beb08d334e8b10bc557f
Andrew Matt <andrewm1246@gmail.com>
2020-12-03 12:46:11 -0500

    Added color display to see whose turn is first

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 let playerOneTurn = false;
 let playerTwoTurn = false;
 
+// Sets random player for start of game and changes colors and text of displays
 function randomPlayer() {
+  let playerOneDisplay = document.querySelector('.player-one-display');
+  let playerTwoDisplay = document.querySelector('.player-two-display');
+
+  //If its less than 0.5, player one's turn, change display one style and text
   if(Math.random() < 0.5) {
     playerOneTurn = true;
-  } else {
+    playerOneDisplay.style.background = '#adce74';
+    playerOneDisplay.querySelector('h1').textContent = "Player One's Turn!"
+  } else { //If its greater than 0.5, player two's turn, change display two style and text
     playerTwoTurn = true;
+    playerTwoDisplay.style.background = '#adce74';
+    playerTwoDisplay.querySelector('h1').textContent = "Player Two's Turn!"
   }
 }
 
 window.onload = function() {
+
   randomPlayer();
   console.log(`Player One: ${playerOneTurn}`);
   console.log(`Player Two: ${playerTwoTurn}`);
